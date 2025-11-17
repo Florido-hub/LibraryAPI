74b70ebd614da940b627f6fd8636f07a573ab6ca
Florido-hub <floridodinizz@gmail.com>
2025-11-17 08:49:41 -0300

    feat: cria repositories de Livro e Autor

---
+++ src/main/java/com/example/LibraryAPI/repository/LivroRepository.java
@@ -0,0 +1,9 @@
+package com.example.LibraryAPI.repository;
+
+import com.example.LibraryAPI.model.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Integer> {
+}
